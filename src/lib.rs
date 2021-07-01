6ec32d2047a3deb4509bb1f1b4798b80c55196da
LambdaXymox <lambda.xymox@gmail.com>
2021-07-01 14:06:51 -0500

    Add NoHashHashMap and NoHashHashSet.

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,26 @@
+extern crate nohash_hasher;
+
+// Re-export hash functions.
+pub use nohash_hasher::*;
+
+use std::collections::{
+    HashMap,
+    HashSet,
+};
+
+
+/// A `HashMap` using a default `NoHashHasher`.
+#[cfg(feature = "std")]
+pub type NoHashHashMap<K, V> = HashMap<K, V, BuildNoHashHasher<K>>;
+
+/// A `HashSet` using a default `NoHashHasher`.
+#[cfg(feature = "std")]
+pub type NoHashHashSet<K, V> = HashSet<V, BuildNoHashHasher<K>>;
+
+
 #[cfg(test)]
 mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+    use super::*;
+    
+    
 }
